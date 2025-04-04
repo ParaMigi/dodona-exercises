3b2513f3533f28ddd74a84289e78f07f567bc5eb
ParaMigi <WWEMikanoA@gmail.com>
2025-04-04 15:13:34 +0200

    Fixed typo in the solution

--- exercises/Greatest common divisor -io/solution/solution.en.py
+++ exercises/Greatest common divisor -io/solution/solution.en.py
@@ -5,4 +5,4 @@ gcd = 1
 for n in range(2, smallest+1):
     if (a % n == 0) and (b % n == 0):
         gcd = n
-print(f"The greates common divisor of {a} and {b} is {gcd}.")
+print(f"The greatest common divisor of {a} and {b} is {gcd}.")
